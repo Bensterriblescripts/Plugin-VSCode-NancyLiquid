d1ff984fd8331c1da3bf0fa4266991a3a6ce7bfa
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2025-10-16 15:36:14 +1300

    created if logic, added some more coloring to tags

--- liquidnancy/src/extension.ts
+++ liquidnancy/src/extension.ts
@@ -23,17 +23,19 @@ const liquidTagDecor = vscode.window.createTextEditorDecorationType({
 });
 
 const re_include = /\binclude\s+['"]([^'"]+)['"]/i;
+const re_block = /\b(block)\s+([a-zA-Z_][\w]*)/i;
+const re_endblock = /\bendblock\b/i;
 const re_assign = /\b(assign)\s+([a-zA-Z_][\w]*)\s*=\s*(.+)/i;
 const re_if = /\b(if)\s+([A-Za-z_]\w*(?:\.[A-Za-z_]\w*)*)\s*(?:==|!=|>|<|>=|<=|contains|and|or)\s*([A-Za-z_]\w*(?:\.[A-Za-z_]\w*)*|'[^']*'|"[^"]*")/i;
 const re_endif = /\bendif\b/i;
 
+const re_number = /^\s*[+-]?(?:\d+(?:\.\d+)?|\.\d+)\s*$/;
 const re_string = /^\s*(["'])(?:\\.|(?!\1).)*\1\s*$/;
-const re_arraykey = /^[a-zA-Z_]\w*\s*\[\s*(['"])[a-zA-Z_][\w]*\1\s*\]$/;
 const re_object = /^\s*(?:this|[A-Za-z_]\w*)(?:\.[A-Za-z_]\w*)+\s*$/;
 
 let variables: {[key: string]: string} = { 
-    "user": "object", "user.id": "string",
-    "request": "object", "request.param": "string",
+    "user": "Object", "user.id": "String",
+    "request": "Object", "request.param": "String",
 };
 let includes: {[key: string]: string} = {};
 
@@ -68,11 +70,13 @@ function lintDocument(collection: vscode.DiagnosticCollection) {
     const liquidDecorations: vscode.DecorationOptions[] = [];
     const liquidTagDecorations: vscode.DecorationOptions[] = [];
 
+    let tagcount = 0;
+    let currenttag: vscode.Range;
+
     for (let linenum = 0; linenum < document.lineCount; linenum++) {
         const line = document.lineAt(linenum).text;
         checkLine(line, linenum);
     }
-
 
     /* Core Function */
     function checkLine(line: string, linenum: number) {
@@ -140,24 +144,18 @@ function lintDocument(collection: vscode.DiagnosticCollection) {
                         }
                     } else if (match = re_assign.exec(liquidtag)) { // Assign
                         if (match[1] && match[2] && match[3]) {
-                            match[3] = match[3].trim();
 
                             if (re_string.test(match[3])) {
-                                variables[match[2]] = "string";
-                                console.log("Added the variable with type string");
+                                variables[match[2]] = "String";
                             } else if (re_object.test(match[3])) {
-                                if (variables[match[3]]) {
-                                    variables[match[2]] = variables[match[3]];
-                                } else {
-                                    variables[match[2]] = "object";
-                                }
+                                variables[match[2]] = "Object";
+                            } else if (re_number.test(match[3])) {
+                                variables[match[2]] = "Number";
                             } else {
-                                if (variables[match[3]]) {
-                                    variables[match[2]] = variables[match[3]];
-                                } else {
-                                    variables[match[2]] = "any";
-                                }
+                                variables[match[2]] = "Any";
                             }
+
+                            console.log("Added variable with the type " + variables[match[2]]);
 
                             // 'assign'
                             const assStart = liquidstart + match[0].indexOf(match[1]) + 1;
@@ -171,57 +169,98 @@ function lintDocument(collection: vscode.DiagnosticCollection) {
                             startPos = new vscode.Position(linenum, varStart);
                             endPos = new vscode.Position(linenum, varStart + match[2].length);
                             matchRange = new vscode.Range(startPos, endPos);
-                            varDecorations.push({ range: matchRange, hoverMessage: variables[match[2]] });
+                            varDecorations.push({ range: matchRange, hoverMessage: "Type: " + variables[match[2]] });
 
                             // Variable
                             const rightStart = liquidstart + match[0].lastIndexOf(match[3]);
                             startPos = new vscode.Position(linenum, rightStart);
                             endPos = new vscode.Position(linenum, rightStart + match[3].length + 1); 
                             matchRange = new vscode.Range(startPos, endPos); 
-                            if (variables[match[2]] === 'object') {
-                                objDecorations.push({range: matchRange });
+                            if (variables[match[2]] === 'Object') {
+                                objDecorations.push({range: matchRange, hoverMessage: "Type: " + "Object" });
+                            } else if (variables[match[2]] === "Any") {
+                                objDecorations.push({range: matchRange, hoverMessage: "Type: " + "Any" });
                             }
 
                         }
-                    } else if (match = re_if.exec(liquidtag)) {
+                    } else if (match = re_if.exec(liquidtag)) { // If Operation
                         if (match[1] && match[2] && match[3]) {
 
                             // "if"
                             const ifStart = liquidstart + match[0].indexOf(match[1]) + 1;
-                            startPos = new vscode.Position(linenum, ifStart); 
+                            startPos = new vscode.Position(linenum, ifStart);
                             endPos = new vscode.Position(linenum, ifStart + match[1].length); 
                             matchRange = new vscode.Range(startPos, endPos); 
                             liquidTagDecorations.push({ range: matchRange });
 
                             // Left Operand
                             const leftStart = liquidstart + match[0].indexOf(match[2]) + 1;
-                            startPos = new vscode.Position(linenum, leftStart); 
+                            startPos = new vscode.Position(linenum, leftStart);
+                            const leftStartPos = startPos;
                             endPos = new vscode.Position(linenum, leftStart + match[2].length); 
                             matchRange = new vscode.Range(startPos, endPos); 
-                            varDecorations.push({ range: matchRange, hoverMessage: variables[match[2]] });
+                            varDecorations.push({ range: matchRange, hoverMessage: "Type: " + variables[match[2]] });
                             if (!variables[match[2]]) {
                                 const diagnostic = new vscode.Diagnostic(matchRange, 'Unable to locate'+match[3]+' in this template', vscode.DiagnosticSeverity.Information);
                                 diagnostics.push(diagnostic);
                             }
 
                             // Right Operand
-                            const rightStart = liquidstart + match[0].lastIndexOf(match[3]);
+                            const rightStart = liquidstart + match[0].lastIndexOf(match[3]) + 1;
                             startPos = new vscode.Position(linenum, rightStart);
-                            endPos = new vscode.Position(linenum, rightStart + match[3].length + 1); 
+                            endPos = new vscode.Position(linenum, rightStart + match[3].length); 
+                            const rightEndPos = endPos;
                             matchRange = new vscode.Range(startPos, endPos); 
-                            varDecorations.push({ range: matchRange, hoverMessage: variables[match[3]] });
+                            varDecorations.push({ range: matchRange, hoverMessage: "Type: " + variables[match[3]] });
                             if (!variables[match[3]]) {
                                 const diagnostic = new vscode.Diagnostic(matchRange, 'Unable to locate '+match[3]+' in this template', vscode.DiagnosticSeverity.Information);
                                 diagnostics.push(diagnostic);
                             }
 
-                            if (variables[match[2]] && variables[match[3]] && variables[match[2]] !== "any" && variables[match[3]] !== "any") {
-                                if (variables[match[2]] !== variables[match[3]]) {
-                                    const diagnostic = new vscode.Diagnostic(matchRange, `Invalid type operation, comparing ${match[2]} (${variables[match[2]]}) against ${match[3]} (${variables[match[3]]})` , vscode.DiagnosticSeverity.Error);
-                                    diagnostics.push(diagnostic);
+                            if (variables[match[2]] && variables[match[3]]) { // Type Operation Error
+                                if (variables[match[2]] !== "Any" && variables[match[3]] !== "Any" && variables[match[2]] !== "Object" && variables[match[3]] !== "Object") {
+                                    matchRange = new vscode.Range(leftStartPos, rightEndPos);
+                                    if (variables[match[2]] !== variables[match[3]]) {
+                                        const diagnostic = new vscode.Diagnostic(matchRange, `Invalid type operation, comparing ${match[2]} (${variables[match[2]]}) against ${match[3]} (${variables[match[3]]})` , vscode.DiagnosticSeverity.Error);
+                                        diagnostics.push(diagnostic);
+                                    }
                                 }
                             }
+
+                            if (tagcount === 0) {
+                                startPos = new vscode.Position(linenum, liquidstart);
+                                endPos = new vscode.Position(linenum, x);
+                                matchRange = new vscode.Range(startPos, endPos);
+                                currenttag = matchRange;
+                            }
+                            tagcount++;
                         }
+                    } else if (re_endif.test(liquidtag)) {
+                        startPos = new vscode.Position(linenum, liquidstart); 
+                        endPos = new vscode.Position(linenum, x); 
+                        matchRange = new vscode.Range(startPos, endPos); 
+                        liquidTagDecorations.push({ range: matchRange });
+                        
+                        tagcount--;
+                    } else if (match = re_block.exec(liquidtag)) { // Block
+                        if (match[1] && match[2]) {
+                            startPos = new vscode.Position(linenum, liquidstart);
+                            endPos = new vscode.Position(linenum, liquidstart + match[1].length + 1);
+                            matchRange = new vscode.Range(startPos, endPos);
+                            liquidTagDecorations.push({ range: matchRange, hoverMessage: "Block Tag" });
+                        }
+                    } else if (re_endblock.test(liquidtag)) {
+                        startPos = new vscode.Position(linenum, liquidstart);
+                        endPos = new vscode.Position(linenum, x);
+                        matchRange = new vscode.Range(startPos, endPos);
+                        liquidTagDecorations.push({ range: matchRange });
+                    }
+                    else {
+                        startPos = new vscode.Position(linenum, liquidstart);
+                        endPos = new vscode.Position(linenum, x);
+                        matchRange = new vscode.Range(startPos, endPos);
+                        const diagnostic = new vscode.Diagnostic(matchRange, `Unknown Tag` , vscode.DiagnosticSeverity.Error);
+                        diagnostics.push(diagnostic);
                     }
 
                     liquidtag = "";
@@ -259,6 +298,11 @@ function lintDocument(collection: vscode.DiagnosticCollection) {
             }
 
         }
+    }
+
+    if (tagcount !== 0 && currenttag) {
+        const diagnostic = new vscode.Diagnostic(currenttag, 'Unclosed `if` Tag', vscode.DiagnosticSeverity.Error);
+        diagnostics.push(diagnostic);
     }
 
     console.log("Variables: ", variables);
